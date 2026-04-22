1ad2d4e97c7195eefeaed190ba6a1995090e7840
priyanka <priyankah288@gmail.com>
2026-04-22 20:14:37 +0530

    static_storage_class

---
+++ Storage_classes_program/Static_storage_class.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void counter()
+{
+    static int count = 0;
+    count++;
+    printf("count = %d\n", count);
+    
+}
+int main()
+{
+   counter();
+   counter();
+    
+    return 0;
+}
