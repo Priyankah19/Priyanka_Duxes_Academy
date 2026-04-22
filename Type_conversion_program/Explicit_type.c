0d72a74b495637afd2eb5a51efcd86b763287559
priyanka <priyankah288@gmail.com>
2026-04-22 20:41:39 +0530

    added_explicittype

---
+++ Type_conversion_program/Explicit_type.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5 , b = 2;
+    float result;
+    
+    result = (float)a / b;
+    
+    printf("Result = %f" , result);
+    
+    return 0;
+}
