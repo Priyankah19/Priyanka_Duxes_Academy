46de6746cc29761fcd2b1b6cabfb849303b2aabf
priyanka <priyankah288@gmail.com>
2026-04-22 20:36:11 +0530

    Added_implicit_conversion

---
+++ Type_conversion_program/Implicit_type_conversion.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    float b = 2.5;
+    float result;
+    
+    result = a + b;
+    
+    printf("Result = %f" , result);
+    
+    return 0;
+}
