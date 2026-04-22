0c77ffa4b64b4b090acd8e1690bb037a3945bddb
priyanka <priyankah288@gmail.com>
2026-04-22 21:17:49 +0530

    added division

---
+++ Simplecalculator_using_variables/Division.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+float num1, num2, result;
+    printf("Enter two numbers: ");
+    scanf("%f %f" ,&num1, &num2);
+    
+    if (num2!= 0) {
+        result = num1 / num2;
+        
+        printf("Division = %.2f\n", result);
+    }else {
+        printf("Error! Division by zero is not allowed \n");
+    }
+    
+    
+    return 0;
+}
