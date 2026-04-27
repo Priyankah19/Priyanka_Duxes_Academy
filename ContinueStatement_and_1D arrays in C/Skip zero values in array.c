31a7b5d39b5e20fe4bfb5c18d4367e264b107f13
priyanka <priyankah288@gmail.com>
2026-04-28 01:45:02 +0530

    added skip zero values in array

---
+++ ContinueStatement_and_1D arrays in C/Skip zero values in array.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] ={10, 0, 20, 0, 30};
+    
+    int i;
+    
+    printf("Valid values:\n");
+
+    for(i = 0;i < 5; i++) {
+        if(arr[i] == 0){
+            continue;
+        }
+         
+            printf("%d ", arr[i]);
+        }
+
+    return 0;
+}
