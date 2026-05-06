8df5cbd8d2415b6cbeb9200fd19ee3912554b24f
priyanka <priyankah288@gmail.com>
2026-05-06 07:42:18 +0530

    added LED ON

---
+++ Call by value_Call by reference/LED ON.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+void ledOn() {
+    printf("LED ON\n");
+}
+
+int main()
+{
+    ledOn();
+
+    return 0;
+}
