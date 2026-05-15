fdbd2d81d0d516a10ac1710d597f82238c81a87f
priyanka <priyankah288@gmail.com>
2026-05-15 11:03:39 +0530

    added student details

---
+++ Structures/struct.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+struct Student {
+    int rollNo;
+    char name[50];
+    float marks;
+};
+
+int main() {
+
+    
+    struct Student s1;
+
+    
+    s1.rollNo = 101;
+    
+    printf("Enter name: ");
+    scanf("%s", s1.name);
+
+    printf("Enter marks: ");
+    scanf("%f", &s1.marks);
+
+
+    printf("\nStudent Details\n");
+    printf("Roll Number: %d\n", s1.rollNo);
+    printf("Name: %s\n", s1.name);
+    printf("Marks: %.2f\n", s1.marks);
+
+    return 0;
+}
