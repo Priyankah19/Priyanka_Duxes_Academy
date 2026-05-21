c9d05d89adbc3c2e6d7a9a6de93589f72f0f52f9
priyanka <priyankah288@gmail.com>
2026-05-21 21:37:33 +0530

    added assesment 21/05/2026

---
+++ Assesment/Assignment 21-05-2026.c
@@ -0,0 +1,41 @@
+#include<stdio.h> 
+#include<unistd.h>
+
+int main()
+{
+    
+    int emergency;
+    while(1)
+    {
+    printf ("RED LIGHT\n");
+    sleep(5);
+    printf  ("YELLOW LIGHT\n");
+    sleep(5);
+    printf ("GREEN LIGHT\n");
+    sleep(5);
+    
+    
+    printf ("Emergency pressed\n");
+    
+   // printf(" press 1 if yes and press 0 if no\n");//
+    
+    scanf("%d", &emergency);
+    
+   while (emergency == 1)
+    {
+       // printf("Emergency detected\n");
+    
+        printf( "RED LIGHT\n");
+        sleep(5);
+        
+         printf ("Emergency pressed\n");// For emergency 0 it should come out of the loop
+         scanf("%d", &emergency);
+    
+     
+        
+    }
+      
+    
+    }
+}
+    
