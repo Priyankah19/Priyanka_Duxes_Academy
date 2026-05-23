99219e3ce2e2de854fac4ac0951cd113e4d1573b
priyanka <priyankah288@gmail.com>
2026-05-23 18:49:46 +0530

    added reverse a name 22-05-2026

---
+++ Assesment/22-05-2026 reverse a name.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char first[20] = "priyanka";
+    char second[20] = "vignesh";
+
+    printf("Original Name : %s %s\n", first, second);
+
+    
+    printf("Reversed Name : %s %s\n", second, first);
+
+    return 0;
+}
+
